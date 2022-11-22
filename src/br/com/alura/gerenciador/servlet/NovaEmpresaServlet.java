2a73e1230fdfbf4c6cb6d555185fa447ad5b412f
dvargas42 <dvargas.eng@gmail.com>
2022-11-22 20:23:11 -0300

    :tada: initial commit

---
+++ src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -0,0 +1,40 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/novaEmpresa")
+public class NovaEmpresaServlet extends HttpServlet {
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Cadastrando nova empresa");
+		
+		String nome = request.getParameter("nome");
+		String paramData = request.getParameter("data");
+		
+		Date dataAbertura;
+		
+		try {
+			SimpleDateFormat standardData = new SimpleDateFormat("dd/MM/yyyy");
+			dataAbertura = standardData.parse(paramData);
+		} catch (ParseException e) {
+			throw new ServletException(e);
+		}
+		
+		Empresa empresa = new Empresa(nome, dataAbertura);
+		
+		Banco banco = new Banco();
+		banco.adiciona(empresa);
+		
+		response.sendRedirect("listaEmpresas");
+	}
+}
