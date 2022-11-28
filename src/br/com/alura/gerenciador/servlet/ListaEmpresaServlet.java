11c90402dc938d69b96173481e040078db6ceb5b
dvargas42 <dvargas.eng@gmail.com>
2022-11-28 15:47:00 -0300

    create serialVersionUID variable

--- src/br/com/alura/gerenciador/servlet/ListaEmpresaServlet.java
+++ src/br/com/alura/gerenciador/servlet/ListaEmpresaServlet.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns = "/listaEmpresas")
 public class ListaEmpresaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 	
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
