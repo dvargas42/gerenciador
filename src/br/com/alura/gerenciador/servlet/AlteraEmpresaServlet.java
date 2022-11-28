6ebf1c247c59ae5c83d6dd58e7fba35f1d352ee6
dvargas42 <dvargas.eng@gmail.com>
2022-11-28 15:36:54 -0300

    :tada: initial commit

---
+++ src/br/com/alura/gerenciador/servlet/AlteraEmpresaServlet.java
@@ -0,0 +1,42 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/alteraEmpresa")
+public class AlteraEmpresaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Alterando empresa");
+		
+		String nome = request.getParameter("nome");
+		String paramData = request.getParameter("data");
+		String paramId = request.getParameter("id");
+		
+		Integer id = Integer.valueOf(paramId);
+		Date dataAbertura = null;
+		
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			dataAbertura = sdf.parse(paramData);
+		} catch (ParseException e){
+			throw new ServletException(e);
+		}
+		
+		Empresa empresa = new Empresa(id, nome, dataAbertura);
+		Banco banco = new Banco();
+		banco.alteraEmpresa(empresa);
+		
+		response.sendRedirect("listaEmpresas");		
+	}
+}
