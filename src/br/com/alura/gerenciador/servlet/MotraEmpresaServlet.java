9cce68eca8734aa599cabb322f8740b737d48591
dvargas42 <dvargas.eng@gmail.com>
2022-11-27 23:02:59 -0300

    :tada: initial commit

---
+++ src/br/com/alura/gerenciador/servlet/MotraEmpresaServlet.java
@@ -0,0 +1,31 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/mostraEmpresa")
+public class MotraEmpresaServlet extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String paramId = request.getParameter("id");
+		Integer id = Integer.valueOf(paramId);
+		
+		Banco banco = new Banco();
+		Empresa empresa = banco.buscaEmpresaPelaId(id);
+		
+		System.out.println(empresa.getNome());
+		
+		request.setAttribute("empresa", empresa);
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/formAlteraEmpresa.jsp");
+		dispatcher.forward(request, response);
+	}
+}
