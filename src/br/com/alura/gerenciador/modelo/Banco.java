3f92382601b9cb1794a783adf8b837aa7ce9cb45
dvargas42 <dvargas.eng@gmail.com>
2022-11-28 22:43:26 -0300

    implement usuario persistence

--- src/br/com/alura/gerenciador/modelo/Banco.java
+++ src/br/com/alura/gerenciador/modelo/Banco.java
@@ -6,15 +6,29 @@ import java.util.List;
 public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<>();
+	private static List<Usuario> listaUsuarios = new ArrayList<>();
+	
 	private static Integer chaveSequencial = 1;
 	
 	static {
 		Empresa empresa = new Empresa(chaveSequencial++, "Alura");
 		Empresa empresa2 = new Empresa(chaveSequencial++, "Caelum");
 		Empresa empresa3 = new Empresa(chaveSequencial++, "Bradesco");
+		
 		lista.add(empresa);
 		lista.add(empresa2);
 		lista.add(empresa3);
+		
+		Usuario u1 = new Usuario();
+		u1.setLogin("nico");
+		u1.setSenha("12345");
+		
+		Usuario u2 = new Usuario();
+		u2.setLogin("daniel");
+		u2.setSenha("12345");
+		
+		listaUsuarios.add(u1);
+		listaUsuarios.add(u2);
 	}
 	
 	public void adiciona(Empresa empresa) {
@@ -55,6 +69,15 @@ public class Banco {
 				empresa.setDataAbertura(emp.getDataAbertura());
 			}
 		}
+	}
+
+	public Usuario existeUsuario(String login, String senha) {
+		for(Usuario usuario: listaUsuarios) {
+			if(usuario.ehIgual(login, senha)) {
+				return usuario;
+			}
+		}
+		return null;
 	}
 }
 
